34018c814f9542d2a2c6b60ff33b2264a173776f
dan-l <daniellimpersonal@gmail.com>
2022-12-20 23:03:46 -0800

    Standalone sign client, need to find a wallet implementing WC V2.0
    Signed-off-by: dan-l <daniellimpersonal@gmail.com>

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 
 ## Web3Modal 
 https://docs.walletconnect.com/2.0/web3modal/about
+
+## reactStrictMode
+`reactStrictMode` in `next.config.js` in development mode will render twice with `useState`.
+See https://nextjs.org/docs/api-reference/next.config.js/react-strict-mode.
+
+## Check which wallet supports WC V2.0
+https://explorer.walletconnect.com/?type=wallet&version=2&page=2
--- components/ConnectStandalone.js
+++ components/ConnectStandalone.js
@@ -1,3 +1,110 @@
+import SignClient from "@walletconnect/sign-client"
+import { Web3Modal } from "@web3modal/standalone"
+
+import { useEffect, useState } from "react"
+
 export default function ConnectStandalone() {
-    
+  const [signClient, setSignClient] = useState(null)
+  const [isConnected, setIsConnected] = useState(false)
+  const [session, setSession] = useState({})
+  const web3Modal = new Web3Modal({
+    projectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID,
+    enableStandaloneMode: true
+  })
+
+  useEffect(() => {
+    async function initClient() {
+      const client = await SignClient.init({
+        projectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID,
+        metadata: {
+          name: "Wallet Connect Integration Dapp",
+          description: "Wallet Connect Integration",
+          url: "http://localhost:3000/",
+          icons: ["https://walletconnect.com/walletconnect-logo.png"],
+        },
+      })
+      setSignClient(client)
+  
+      client.on("session_event", ({ event }) => {
+        // Handle session events, such as "chainChanged", "accountsChanged", etc.
+      })
+  
+      client.on("session_update", ({ topic, params }) => {
+        const { namespaces } = params
+        const _session = client.session.get(topic)
+        // Overwrite the `namespaces` of the existing session with the incoming one.
+        const updatedSession = { ..._session, namespaces }
+        // Integrate the updated session state into your dapp state.
+        setSession(updatedSession);
+      })
+  
+      client.on("session_delete", () => {
+        // Session was deleted -> reset the dapp state, clean up from user session, etc.
+        setIsConnected(false)
+      })
+    }
+
+    initClient()
+  }, [SignClient])
+
+  const open = async () => {
+    // NOTE: THIS ONLY WORKS WITH WALLET SUPPORTING WC V2.0
+    try {
+      const { uri, approval } = await signClient.connect({
+        pairingTopic: signClient.core.pairing.getPairings()?.topic,
+        requiredNamespaces: {
+          eip155: {
+            methods: [
+              "eth_sendTransaction",
+              "eth_signTransaction",
+              "eth_sign",
+              "personal_sign",
+              "eth_signTypedData",
+            ],
+            chains: ["eip155:1", "eip155:5"],
+            events: ["chainChanged", "accountsChanged"],
+          }
+        },
+      })
+  
+      if (uri) {
+        // mainnet, goerli
+        const standaloneChains = ["eip155:1", "eip155:5"]
+        web3Modal.openModal({ uri, standaloneChains })
+        const session = await approval()
+        setSession(session)
+        web3Modal.closeModal()
+      }
+    } catch (e) {
+      console.error('Error pairing', e)
+    }
+  }
+
+
+  return (
+    <>
+      {signClient ?
+        <div>
+          {isConnected ?
+            (
+              <>
+                <div>
+                  {`Connected to: ${address} (${chain.name})`}
+                </div>
+                <code>
+                  {JSON.stringify(session, null, 2)}
+                </code>
+              </>
+            ) :
+            (<button onClick={open}>
+              Connect
+            </button>
+            )
+          }
+        </div>
+        :
+        <h1>Not ready</h1>
+      }
+    </>
+  )
 }
